12a1f5813bf15665f5b64ffb75f666435b02429a
diffficult <spaulo@gmail.com>
2019-04-10 14:14:11 -0300

    updated config and st to 0.8.1

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "tewi:pixelsize=8:antialias=true";
-/* static char *font = "Tewi:pixelsize=8:antialias=true:autohint=true"; */
+/* static char *font = "scientifica:pixelsize=8:antialias=true:autohint=true";  */
 static int borderpx = 2;
 
 /*
